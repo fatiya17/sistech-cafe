690b6116f6a5760452c755ce3cd8b0bdaeadbcda
fatiya17 <fatiyalabibah17@gmail.com>
2026-06-16 20:39:32 +0700

    feat: update product grid column when mobile and tablet

--- components/menu/ProductGrid.jsx
+++ components/menu/ProductGrid.jsx
@@ -167,7 +167,7 @@ export function ProductGrid() {
       </form>
 
       {/* Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 gap-y-10">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 gap-y-8 md:gap-y-10">
         {mockProducts.map((product) => (
           <ProductCard
             key={product.id}
